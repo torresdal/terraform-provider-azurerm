eab7173696534e62c29dd265be8a174305367699
tombuildsstuff <git@ibuildstuff.co.uk>
2018-06-22 11:43:16 +0200

    Storage Account: exposing `(primary|secondary)_blob_domain` to avoid hacking via HCL

--- azurerm/resource_arm_storage_account.go
+++ azurerm/resource_arm_storage_account.go
@@ -3,6 +3,7 @@ package azurerm
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"regexp"
 	"strings"
 
@@ -184,6 +185,16 @@ func resourceArmStorageAccount() *schema.Resource {
 			},
 
 			"secondary_location": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"primary_blob_domain": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"secondary_blob_domain": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -644,6 +655,13 @@ func resourceArmStorageAccountRead(d *schema.ResourceData, meta interface{}) err
 			pscs := fmt.Sprintf("DefaultEndpointsProtocol=https;BlobEndpoint=%s;AccountName=%s;AccountKey=%s",
 				*endpoints.Blob, *resp.Name, *accessKeys[0].Value)
 			d.Set("primary_blob_connection_string", pscs)
+
+			uri, err := url.Parse(*endpoints.Blob)
+			if err != nil {
+				return fmt.Errorf("Error parsing Primary Blob Endpoint as a URI: %+v", err)
+			}
+
+			d.Set("primary_blob_domain", uri.Host)
 		}
 
 		if endpoints := props.SecondaryEndpoints; endpoints != nil {
@@ -668,6 +686,13 @@ func resourceArmStorageAccountRead(d *schema.ResourceData, meta interface{}) err
 			} else {
 				d.Set("secondary_table_endpoint", "")
 			}
+
+			uri, err := url.Parse(*endpoints.Blob)
+			if err != nil {
+				return fmt.Errorf("Error parsing Secondary Blob Endpoint as a URI: %+v", err)
+			}
+
+			d.Set("secondary_blob_domain", uri.Host)
 		}
 
 		networkRules := props.NetworkRuleSet
